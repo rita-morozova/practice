7a441c9b7b8f462750271c9bbe10c30f907b4c78
Margarita Morozova <ritamorozova@yahoo.com>
2021-05-14 20:37:37 -0700

    added webpack config and dependencies

--- shape-tracker/src/main.js
+++ shape-tracker/src/main.js
@@ -1,3 +1,9 @@
+import Triangle from './triangle.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+
 $(document).ready(function() {
     $('#triangle-checker-form').submit(function(event) {
       event.preventDefault();
